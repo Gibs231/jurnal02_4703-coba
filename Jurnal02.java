28f7c383f6db0f7ad8733ec02859aa26fb5e97e1
Gibs321 <ramlahhendri123@gmail.com>
2023-09-27 18:27:40 +0700

    tambahkan output

--- Jurnal02.java
+++ Jurnal02.java
@@ -1,5 +1,7 @@
 public class Jurnal02{
     public static void main(String[] args) {
-        System.out.println(Hello Gibs!!!);
+        System.out.println(Muhamad Gibraltar);
+        System.out.println(607062330123);
+        System.out.println(03);
     }
 }
